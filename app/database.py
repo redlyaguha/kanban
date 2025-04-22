c4595dd3af16f53a52a6e0b99259b028233ddbee
lyaguha <hellery_yt@bk.ru>
2025-04-23 00:34:10 +0300

    Cannot find reference 'get_db' in 'database.py'
    +Генератор сессий БД

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from sqlalchemy import create_engine
 
+
 # Формат строки подключения для SQL Server:
 # mssql+pyodbc://<username>:<password>@<server>/<database>?driver=ODBC+Driver+17+for+SQL+Server
 DATABASE_URL = "mssql+pyodbc://@localhost/kanban?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
@@ -10,3 +11,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
     pass
+#Генератор сессий БД
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
